29c319ed87a5cbef31d0b668de8273f1b74ce78a
Selwyn Onedo <selwyn.onedo14@gmail.com>
2023-10-11 11:59:21 +0800

    change active win

--- Midterm/Midterm/index.js
+++ Midterm/Midterm/index.js
@@ -83,15 +83,31 @@ v.addListener(calledOnce);
 
 const activeWindow = require('active-win');
 
-(async () => {
+let currentWindowTitle = ""; // Store the title of the currently active window/tab
+
+// Function to update the active window information
+async function updateActiveWindowInfo() {
     try {
         const windowInfo = await activeWindow();
-        const formattedInfo = `Title: ${windowInfo.title}, ID: ${windowInfo.id}, Bounds: ${JSON.stringify(windowInfo.bounds)}, Owner: ${windowInfo.owner.name}, URL: ${windowInfo.url}, Memory Usage: ${windowInfo.memoryUsage}`;
-        console.log(formattedInfo);
+        const newWindowTitle = windowInfo.title;
+
+        if (newWindowTitle !== currentWindowTitle) {
+            // The user has changed tabs or windows, update the information
+            console.clear(); // Clear the console to update the output
+            console.log("Active Window:", newWindowTitle);
+
+            // Update the current window title
+            currentWindowTitle = newWindowTitle;
+        }
     } catch (error) {
         console.error("Error:", error);
     }
-})();
+}
+
+// Set an interval to periodically check the active window
+const interval = 1000; // Check every 1 second
+setInterval(updateActiveWindowInfo, interval);
+
 
 
 
