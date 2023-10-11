a1b7b5fca5f26322ae8b4b2c2a4221173e7120f1
Selwyn Onedo <selwyn.onedo14@gmail.com>
2023-10-11 12:00:53 +0800

    remove unnecessary comments

--- Midterm/Midterm/index.js
+++ Midterm/Midterm/index.js
@@ -40,47 +40,6 @@ calledOnce = function (e) {
 v.addListener(calledOnce);
 
 
-
-// Group 1 - Keylogger 
-// var GlobalKeyboardListener = require("node-global-key-listener").GlobalKeyboardListener;
-
-// const v = new GlobalKeyboardListener();
-
-// //Log every key that's pressed.
-// v.addListener(function (e, down) {
-//     console.log(
-//         `${e.name} ${e.state == "DOWN" ? "DOWN" : "UP  "} [${e.rawKey._nameRaw}]`
-        
-//     );
-// })
-
-// //Capture Windows + Space on Windows and Command + Space on Mac
-// v.addListener(function (e, down) {
-//     if (
-//         e.state == "DOWN" &&
-//         e.name == "SPACE" &&
-//         (down["LEFT META"] || down["RIGHT META"])
-//     ) {
-//         //call your function
-//         return true;
-//     }
-// });
-
-// //Capture ALT + F
-// v.addListener(function (e, down) {
-//     if (e.state == "DOWN" && e.name == "F" && (down["LEFT ALT"] || down["RIGHT ALT"])) {
-//         //call your function
-//         return true;
-//     }
-// });
-
-// //Call one listener only once (demonstrating removeListener())
-// calledOnce = function (e) {
-//     console.log("only called once");
-//     v.removeListener(calledOnce);
-// };
-// v.addListener(calledOnce);
-
 const activeWindow = require('active-win');
 
 let currentWindowTitle = ""; // Store the title of the currently active window/tab
@@ -107,42 +66,3 @@ async function updateActiveWindowInfo() {
 // Set an interval to periodically check the active window
 const interval = 1000; // Check every 1 second
 setInterval(updateActiveWindowInfo, interval);
-
-
-
-
-// const activeWindow = require('active-win');
-
-// (async () => {
-// 	console.log(await activeWindow(options)), {
-//         title: 'Keyloggers-Group 1',
-//         id: 5762,
-//         bounds: {
-//             x: 0,
-//             y: 0,
-//             height: 900,
-//             width: 1440
-//         },
-//         owner: {
-//             name: 'Google Chrome',
-//             processId: 310,
-//             bundleId: 'com.google.Chrome',
-//             path: '/Applications/Google Chrome.app'
-//         },
-//         url: 'https://sindresorhus.com/unicorn',
-//         memoryUsage: 11015432
-//     }});
-
-    /*
-To add logging of errors please use. This is hopefully not needed in most cases, but may still be useful in production.
-
-new GlobalKeyboardListener({
-        windows: {
-            onError: (errorCode) => console.error("ERROR: " + errorCode),
-            onInfo: (info) => console.info("INFO: " + info)
-        },
-        mac: {
-            onError: (errorCode) => console.error("ERROR: " + errorCode),
-        }
-    }); 
-*/
